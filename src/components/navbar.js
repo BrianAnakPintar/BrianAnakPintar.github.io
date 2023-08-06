fd50b6569d5adc2ef9b9b3d6f83b570097a43b51
Brian <brianfransis.moniaga@gmail.com>
2023-08-06 12:04:43 -0700

    Animations added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import ResumeButton from "./resume-button";
-import logo from  "../logo.png";
+import {motion} from "framer-motion";
 
-function Navbar(props) {
+function Navbar() {
     const [sticked, passed] = useState(false)
 
     function hasPassed()
@@ -23,11 +23,14 @@ function Navbar(props) {
     return (
         <header className={sticked? `nav-header sticked` : `nav-header`}>
             <nav>
-                <ul className="nav-links">
+                <motion.ul className="nav-links"
+                           initial={{ opacity: 0, x:100 }}
+                           animate={{ opacity: 1, x: 0 }}
+                           transition={{ duration: .5 }}>
                     <li><a href="#projects" className="hover-underline-animation">Projects</a></li>
                     <li><a href="#skills" className="hover-underline-animation">Skills</a></li>
                     <li><a href="#contact" className="hover-underline-animation">Contact</a></li>
-                </ul>
+                </motion.ul>
             </nav>
             <ResumeButton/>
         </header>
